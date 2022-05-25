a60106adb85aff1a28ebe9d89ee5ed051424bea8
Justin-Gb <59418416+Justin-Gb@users.noreply.github.com>
2022-05-25 01:36:46 -0700

    Add files via upload

---
+++ clustering.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def distance(u,v,p):
+    return np.mean(abs(u-v)**p,axis = 1)
+  
+def wkmeans(X,p=1,k=2,tol=1e-8,state=200,max_iter = 500,verbose = True, k_means_pp = True):
+    
+    """
+    X: M x N matrix with M log_ret time_series
+    k: Number of clusters
+    p: Wp-metric
+    Return: (k x N matrix that represents k centroids, array of M labels)
+    """
+    
+    X_sorted = np.sort(X,axis = 1)
+    
+    rng = np.random.default_rng(state)
+    
+    if (k_means_pp):
+        barycenters_0 = X_sorted[rng.choice(X.shape[0])].reshape(1,-1)
+        
+        for i in range(k-1):
+            min_d = np.array([distance(X_sorted,barycenters_0[i],p) for i in range(barycenters_0.shape[0])]).min(axis = 0)
+            barycenters_0 = np.vstack([barycenters_0,X_sorted[min_d.argmax()]])                  
+
+    else:
+        barycenters_0 = X_sorted[rng.choice(X.shape[0],k,replace=False)]
+        
+        
+    barycenters_1 = None
+    cluster = rng.integers(0,k,X.shape[0])
+    counter = 0
+    
+
+    while (counter <= max_iter):
+        
+        cluster = np.array([distance(X_sorted,barycenters_0[i],p) for i in range(k)]).argmin(axis = 0)
+        
+        if (p == 1):
+            
+            barycenters_1 = np.array([np.median(X_sorted[cluster == i], axis = 0) for i in range(k)])
+
+        elif (p > 1):
+            
+            barycenters_1 = np.array([np.mean(X_sorted[cluster == i], axis = 0) for i in range(k)]) 
+            
+        counter += 1
+        
+        loss = np.sum([distance(X_sorted[cluster == i],barycenters_1[i],p).sum() for i in range(k)])
+        
+        if (verbose):
+            
+            print("Mean Loss after {} iterations: {}".format(counter,loss/len(X)))
+        
+        
+        if (sum(distance(barycenters_0,barycenters_1,p)) < tol):
+        
+            print("Convergence Reached! Number of Iterations: {}".format(counter))
+            
+            break
+        
+        else:
+            
+            barycenters_0 = barycenters_1.copy()
+            
+
+    if(counter == max_iter):
+        
+        print('Maximum Number of Iteration Reached!')
+    
+    return barycenters_1, cluster

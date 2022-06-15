9d66288b6720ba19df29957c721b9a6c566b2fea
Justin Cheung <59418416+ccyccyjustin@users.noreply.github.com>
2022-06-15 15:59:29 -0700

    Update transform.py

--- algo/transform.py
+++ algo/transform.py
@@ -17,16 +17,16 @@ def lift(y,h1,h2):
     arr = y.values
     return arr[ind]
 
-def transform_kmeans_labels(labels,h1,h2,k):
-    
-    """
-    Input: the label of sliding windows from a k-means type algorithm
-    k: no. of clusters
-    Note it works only if h2 divides h1
-    Return: the frequency of label for original time series with complete votes, k columns
-    """
-    m = len(labels) - h1//h2 + 1
-    count = np.array([np.sum(labels[np.arange(h1//h2)[None,:] + np.arange(m)[:,None]] == i,axis = 1) for i in range(k)])
-    #[[0,1,2,...,h1//h2-1],[1,2,3,...h1//h2],...[m,m+1,...m+h1//h2-1]]
-    count = np.tile(count.T,h2).reshape(-1,k) # count is the same for the same h2 rows
-    return np.concatenate([np.zeros((h1-h2,k)),count,np.zeros((h1-h2,k))]) # Incomplete votes will be filled with 0
+#def transform_kmeans_labels(labels,h1,h2,k):
+#    
+#    """
+#    Input: the label of sliding windows from a k-means type algorithm
+#    k: no. of clusters
+#    Note it works only if h2 divides h1
+#    Return: the frequency of label for original time series with complete votes, k columns
+#    """
+#    m = len(labels) - h1//h2 + 1
+#    count = np.array([np.sum(labels[np.arange(h1//h2)[None,:] + np.arange(m)[:,None]] == i,axis = 1) for i in range(k)])
+#    #[[0,1,2,...,h1//h2-1],[1,2,3,...h1//h2],...[m,m+1,...m+h1//h2-1]]
+#    count = np.tile(count.T,h2).reshape(-1,k) # count is the same for the same h2 rows
+#    return np.concatenate([np.zeros((h1-h2,k)),count,np.zeros((h1-h2,k))]) # Incomplete votes will be filled with 0
